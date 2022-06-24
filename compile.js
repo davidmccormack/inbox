2247ac1d0d56306600ddd3a15babd4bce9fc2259
David Gutierrez-McCormack <davidmccormack2000@gmail.com>
2022-06-24 01:30:05 -0400

    Added compile file.

---
+++ compile.js
@@ -0,0 +1,13 @@
+// we can't simply import our smart contract because it is solidity code, not javascript
+// so node does not know how to import it.
+// thus, we have to use a filestream.
+const path = require('path');
+const fs = require('fs');
+
+// __dirname is a node variable that contains the project directory.
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+// import solidity compiler and compile contact.
+const solc = require('solc');
+console.log(solc.compile(source, 1));
