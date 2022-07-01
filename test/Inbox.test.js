2911102fbedb0d69790d74691000cb07c9d9cd8a
David Gutierrez-McCormack <davidmccormack2000@gmail.com>
2022-06-30 18:13:05 -0700

    Added contract deployment to tests.

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,8 @@
 // testing library
 const assert = require('assert');
 
-// tool to run a local ethereum network
+// tool to run a local ethereum network and deploy our contract
+// supplies us with test accounts that we can use
 const ganache = require('ganache-cli');
 
 // library for interacting with smart contract's ABI via JavaScript
@@ -12,5 +13,41 @@ const Web3 = require('web3')
 // (could be a local ganache network or the real eth network).
 const web3 = new Web3(ganache.provider());
 
+/*
+* Test structure is as follows
+* 1. Deploy contract in a beforeEach
+* 2. Modify contract
+* 3. Perform Assertions
+* */
 
+
+// Get compiled contract interface & bitecode from our compiled contract
+const { interface, bytecode } = require('../compile');
+let _accounts;
+let _inbox;
+
+beforeEach(async () => {
+    // Get a list of all accounts
+    _accounts = await web3.eth.getAccounts();
+
+
+    // Use one of those accounts to deploy the contract
+    const deployOptions = {
+        data: bytecode,
+        arguments: ['Hi there!']
+    }
+    _inbox = await new
+        web3.eth.Contract(JSON.parse(interface))            // loads interface into a new contract
+        .deploy(deployOptions)                              // loads bitecode and initial value into contract
+        .send({from: _accounts[0], gas:1000000})    // creates a transaction to append contract to blockchain.
+                                                            // _inbox = reference to inbox contract.
+})
+
+describe('Inbox', function () {
+
+    it('should deploy the Inbox contract', () => {
+        console.log(_inbox);
+    })
+
+});
 
